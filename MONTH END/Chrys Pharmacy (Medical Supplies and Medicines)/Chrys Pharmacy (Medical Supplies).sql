a809d102a76c6e853feeb03b536ae6f78d35d69c
ressamartinez <40679874+ressamartinez@users.noreply.github.com>
2018-08-09 14:20:40 +0800

    Update Chrys Pharmacy (Medical Supplies).sql

--- MONTH END/Chrys Pharmacy (Medical Supplies and Medicines)/Chrys Pharmacy (Medical Supplies).sql
+++ MONTH END/Chrys Pharmacy (Medical Supplies and Medicines)/Chrys Pharmacy (Medical Supplies).sql
@@ -11,7 +11,9 @@ SELECT hn,
 	   temp.discount_amount,
 	   temp.gross_amount,
 	   temp.visit_type_rcd,
-	   temp.transaction_date_time,
+	   temp.transaction_date_time as [Transaction DateTime],
+	   CONVERT(VARCHAR(20), temp.transaction_date_time,101) AS [Invoice Date],
+		FORMAT(temp.transaction_date_time,'hh:mm tt') AS [Invoice Time],
 	   temp.visit_type,
 	   temp.item_code,
 	   temp.item_name,
@@ -77,3 +79,4 @@ from
 			and i.item_id = cid.item_id
 			and gac.gl_acct_code_code = '1160201'
 ) as temp
+order by temp.transaction_date_time

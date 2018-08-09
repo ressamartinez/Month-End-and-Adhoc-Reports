67f4f87618b7e4cd89463a3a9fdab9ade4c75fc4
ressamartinez <40679874+ressamartinez@users.noreply.github.com>
2018-08-09 14:20:10 +0800

    Update Charge Count per Charge Type, Item Group, Item with added Service Provider.sql

--- MONTH END/Charge Count per Charge Type, Item Group/Charge Count per Charge Type, Item Group, Item with added Service Provider.sql
+++ MONTH END/Charge Count per Charge Type, Item Group/Charge Count per Charge Type, Item Group, Item with added Service Provider.sql
@@ -1,4 +1,5 @@
---6985 / 7243 / 7242
+--CHARGE COUNT PER CHARGE TYPE, ITEM GROUP, ITEM with added SERVICE PROVIDER
+
 SELECT
 	b.charge_type_rcd,
 	main_item_group = (SELECT
@@ -15,28 +16,17 @@ SELECT
 	c.item_code,
 	c.name_l AS item_desc,
 	SUM(a.quantity) AS qty
-	--,cc.name_l
-	--,(SELECT
-	--				name_l
-	--				FROM AmalgaPRODdbo.swe_payment_status_ref
-	--				WHERE swe_payment_status_rcd = a.payment_status_rcd) as payment_status
-	--,(SELECT
-	--				name_l
-	--				FROM AmalgaPRODdbo.item_type_ref
-	--				WHERE item_type_rcd = c.item_type_rcd) as item_type
 	,(SELECT DISTINCT name_l from costcentre where costcentre_id = a.service_provider_costcentre_id) as service_provider
 
 FROM	AmalgaPROD.dbo.charge_detail_nl_view a,
 			AmalgaPROD.dbo.patient_visit_nl_view b,
 			AmalgaPROD.dbo.item_nl_view c
-			--LEFT JOIN AmalgaPRODdbo.item_group_costcentre igc on c.item_group_id = igc.item_group_id 
-			--LEFT JOIN AmalgaPRODdbo.costcentre cc on igc.costcentre_id = cc.costcentre_id
 
 WHERE a.patient_visit_id = b.patient_visit_id
 AND a.item_id = c.item_id
 AND (
-		MONTH(a.charged_date_time) = 05
-		AND YEAR(a.charged_date_time) = 2018
+		MONTH(a.charged_date_time) = @Month
+		AND YEAR(a.charged_date_time) = @Year
 		)
 AND a.deleted_date_time IS NULL
 AND a.patient_visit_package_id IS NULL
@@ -46,11 +36,8 @@ GROUP BY	b.charge_type_rcd,
 					c.item_group_id,
 					c.item_code,
 					c.name_l 
-					--,cc.name_l
 					,a.service_provider_costcentre_id
-					--,a.payment_status_rcd
-					--,c.item_type_rcd
-					
+
 					UNION 
 					
 SELECT
@@ -69,34 +56,17 @@ SELECT
 	c.item_code,
 	c.name_l AS item_desc,
 	SUM(a.quantity) AS qty
-	--,costcentre = (SELECT
-	--				name_l
-	--				FROM AmalgaPRODdbo.costcentre
-	--				WHERE costcentre_id = (SELECT
-	--																item_group_id
-	--																FROM AmalgaPRODdbo.item_group_costcentre
-	--																WHERE item_group_id = c.item_group_id))
-	--,(SELECT
-	--				name_l
-	--				FROM AmalgaPRODdbo.swe_payment_status_ref
-	--				WHERE swe_payment_status_rcd = a.payment_status_rcd) as payment_status
-	--,(SELECT
-	--				name_l
-	--				FROM AmalgaPRODdbo.item_type_ref
-	--				WHERE item_type_rcd = c.item_type_rcd) as item_type
 	,(SELECT DISTINCT name_l from costcentre where costcentre_id = a.service_provider_costcentre_id) as service_provider
 
 FROM	AmalgaPROD.dbo.charge_detail_nl_view a,
 			AmalgaPROD.dbo.patient_visit_nl_view b,
 			AmalgaPROD.dbo.item_nl_view c
-			--LEFT JOIN AmalgaPRODdbo.item_group_costcentre igc on c.item_group_id = igc.item_group_id 
-			--LEFT JOIN AmalgaPRODdbo.costcentre cc on igc.costcentre_id = cc.costcentre_id
 
 WHERE a.patient_visit_id = b.patient_visit_id
 AND a.item_id = c.item_id
 AND (
-		MONTH(a.charged_date_time) = 05
-		AND YEAR(a.charged_date_time) = 2018
+		MONTH(a.charged_date_time) = @Month
+		AND YEAR(a.charged_date_time) = @Year
 		)
 AND a.deleted_date_time IS NULL
 AND patient_visit_package_id IS NOT NULL
@@ -107,5 +77,3 @@ GROUP BY	b.charge_type_rcd,
 			c.item_code,
 			c.name_l
 			,a.service_provider_costcentre_id
-			--,a.payment_status_rcd
-			--,c.item_type_rcd

c078b47bf3d5f94d35adba59b13222827c707ced
ressamartinez <40679874+ressamartinez@users.noreply.github.com>
2018-08-09 11:44:22 +0800

    Update ACICollateralRevenue-Format2.sql

--- MONTH END/ACI Collateral Revenue/ACICollateralRevenue-Format2.sql
+++ MONTH END/ACI Collateral Revenue/ACICollateralRevenue-Format2.sql
@@ -2,9 +2,9 @@ DECLARE @iMonth int
 DECLARE @iMonth2 int
 DECLARE @iYear int
 
-SET @iMonth =  10
-SET @iMonth2 = 10
-SET @iYear = 2017
+SET @iMonth =  @MonthFrom
+SET @iMonth2 = @MonthTo
+SET @iYear = @Year
 
 SELECT invoice_no as [Invoice No.],
 	   invoice_date as [Invoice Date],
@@ -22,7 +22,7 @@ SELECT invoice_no as [Invoice No.],
 	   palliative_care as [Palliative Care],
 	   home_care as [Home Care],
 	   pain_management as [Pain Management Clinic]
-from rpt_aci_visit_summary2
+from HISReport.dbo.rpt_aci_visit_summary2
 where  MONTH(invoice_date) >= @iMonth and MONTH(invoice_date) <= @iMonth2
     and YEAR(invoice_date) = @iYear
     and gl_acct_code_code not in ('2154940','4445000')

056c78fe0f84aad1c8ed54e846cf9770bbc3a402
ressamartinez <40679874+ressamartinez@users.noreply.github.com>
2018-08-14 11:46:27 +0800

    Update Validated procedures of Cardiologist in Heart Station.sql

--- MONTH END/Validated procedures of Cardiologist in Heart Station/Validated procedures of Cardiologist in Heart Station.sql
+++ MONTH END/Validated procedures of Cardiologist in Heart Station/Validated procedures of Cardiologist in Heart Station.sql
@@ -1,8 +1,8 @@
 SELECT
 	*
 FROM dis.df_browse_validated
-WHERE month(charge_date) = 7
-and year(charge_date) = 2018
+WHERE month(charge_date) = @Month
+and year(charge_date) = @Year
 --charge_date BETWEEN '07-01-2018 00:00:00.000' and '07-31-2018 23:59:59.998'
 and costcentre_group_id = '8A6503A8-39EE-49B7-8455-8343F0A4F290'   --Heart Station
 and validated = 'yes'

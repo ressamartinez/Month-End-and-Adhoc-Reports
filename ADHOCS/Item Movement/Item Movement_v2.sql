c745f376b3391a30a5d8a8abe03a0bda6561c6b8
ressamartinez <40679874+ressamartinez@users.noreply.github.com>
2020-03-30 08:40:55 +0800

    Add files via upload

--- ADHOCS/Item Movement/Item Movement_v2.sql
+++ ADHOCS/Item Movement/Item Movement_v2.sql
@@ -1,3 +1,9 @@
+DECLARE @dFrom datetime
+DECLARE @dTo datetime
+
+SET @dFrom = /*@From*/ '01/01/2018 00:00:00.000'		--Jan 2018 = 22667
+SET @dTo = /*@To*/ '09/30/2019 23:59:59.998'
+
 
 Select s.name_l as [Store Name],
 	   ig.name_l as [Item Group Name],
@@ -13,7 +19,20 @@ Select s.name_l as [Store Name],
 	   im.qty_on_hand as [On Hand],
 	   iml.lot_number as [Lot Number],
 	   im.date_time as [Date/Time],
-	   pfni.display_name_l as [User Name]
+	   pfni.display_name_l as [User Name],
+	   [Last Purchased Date] = (select top 1 sdp.ordered_on_date 
+							from swe_delivery_performance sdp
+							where sdp.item_id = im.item_id
+							and cast(convert(varchar(10),sdp.ordered_on_date,101)as smalldatetime) <= cast(convert(varchar(10),@dTo,101)as smalldatetime)
+							order by sdp.ordered_on_date desc),
+	   [Last Delivery Date] = (select top 1 received_on_date 
+							from swe_delivery_performance sdp
+							where sdp.item_id = im.item_id
+							and cast(convert(varchar(10),sdp.received_on_date,101)as smalldatetime) <= cast(convert(varchar(10),@dTo,101)as smalldatetime)
+							order by sdp.received_on_date desc)
+	   --sdp.ordered_on_date
+	   --,sdp.received_on_date
+
 from item_movement im
 		 left join item i on i.item_id = im.item_id 
 		 left join store s on s.store_id = im.store_id
@@ -23,14 +42,16 @@ from item_movement im
 		 left join item_movement_type_ref imtr on imtr.item_movement_type_rcd = im.item_movement_type_rcd
 		 left join uom_ref ur on ur.uom_rcd = i.uom_rcd
 		 left join item_group ig on i.item_group_id = ig.item_group_id
+		 --left join swe_delivery_performance sdp on im.item_id = sdp.item_id
+
 where im.store_id = --'08CD273C-19CE-11DA-A79E-001143B8816C'           --Central Warehouse,  
                      '08CD273F-19CE-11DA-A79E-001143B8816C'          --POS
 	  and ig.item_group_id = 'F4678B58-5C74-4B6E-A527-83C5BF98032C'    --OR Supplies - Exclusive
-      and CAST(CONVERT(VARCHAR(10),im.date_time,101) as SMALLDATETIME) >= CAST(CONVERT(VARCHAR(10),'07/01/2018',101) as SMALLDATETIME)
-	  and CAST(CONVERT(VARCHAR(10),im.date_time,101) as SMALLDATETIME) <= CAST(CONVERT(VARCHAR(10),'03/31/2019',101) as SMALLDATETIME)
-	  --and year(im.date_time) = @Year
-	  --and month(im.date_time) BETWEEN @MonthFrom AND @MonthTo
+      and CAST(CONVERT(VARCHAR(10),im.date_time,101) as SMALLDATETIME) >= CAST(CONVERT(VARCHAR(10),@dFrom,101) as SMALLDATETIME)
+	  and CAST(CONVERT(VARCHAR(10),im.date_time,101) as SMALLDATETIME) <= CAST(CONVERT(VARCHAR(10),@dTo,101) as SMALLDATETIME)
 	  --and year(im.date_time) = 2018
 	  --and month(im.date_time) BETWEEN 1 AND 7
-	  and imtr.item_movement_type_rcd IN ('ID', 'SDEL', 'S')  --movement type
+	  and imtr.item_movement_type_rcd IN (/*'ID', 'SDEL',*/ 'S')  --movement type
+	  --and i.item_code = '588-0024'
+
 order by [Item Name], [Date/Time]

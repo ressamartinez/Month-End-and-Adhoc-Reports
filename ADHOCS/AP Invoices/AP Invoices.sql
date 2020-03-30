aa387af90e9bf8ea466e105ae5de1676379455c3
ressamartinez <40679874+ressamartinez@users.noreply.github.com>
2020-03-30 08:30:02 +0800

    Update AP Invoices.sql

--- ADHOCS/AP Invoices/AP Invoices.sql
+++ ADHOCS/AP Invoices/AP Invoices.sql
@@ -1,9 +1,11 @@
 
 
-SELECT 
+SELECT gac.gl_acct_code_code as [GL Account Code],
+	  gac.name_l as [GL Account Name],
 	  tsr.name_l as [Transaction Status],
 	  sat.system_transaction_type_rcd as [Transaction Type],
 	  glt.transaction_text as [Transaction No.],
+	  --sat.transaction_text,
 	  sat.vendor_invoice_no as [Vendor Invoice No.],
 	  sat.swe_payment_status_rcd as [Payment Status],
 	  sat.currency_rcd as Currency,
@@ -34,8 +36,8 @@ where
 			AND sat.user_transaction_type_id in ('30957F9D-735D-11DA-BB34-000E0C7F3ED2','E27021D9-8F5A-11DE-B6C3-00237DBC514A')		--APINV, IAP
 			AND glt.transaction_status_rcd NOT IN ('VOI', 'UNK')
 			AND sat.transaction_status_rcd NOT IN ('VOI', 'UNK')
-			and CAST(CONVERT(VARCHAR(10),sat.vendor_invoice_date_time,101) as SMALLDATETIME) >= CAST(CONVERT(VARCHAR(10),'06/15/2018',101) as SMALLDATETIME)
-			and CAST(CONVERT(VARCHAR(10),sat.vendor_invoice_date_time,101) as SMALLDATETIME) <= CAST(CONVERT(VARCHAR(10),'07/31/2018',101) as SMALLDATETIME)
+			and CAST(CONVERT(VARCHAR(10),sat.vendor_invoice_date_time,101) as SMALLDATETIME) >= CAST(CONVERT(VARCHAR(10),'11/16/2019',101) as SMALLDATETIME)
+			and CAST(CONVERT(VARCHAR(10),sat.vendor_invoice_date_time,101) as SMALLDATETIME) <= CAST(CONVERT(VARCHAR(10),'01/15/2020',101) as SMALLDATETIME)
 
 
 order BY sat.vendor_invoice_date_time
